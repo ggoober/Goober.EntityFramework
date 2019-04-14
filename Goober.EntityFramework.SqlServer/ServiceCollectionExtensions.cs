ada72a76a2fd42582858feb4ab8cd90ded07a68c
DTskhai <trongood@mail.ru>
2019-04-14 22:08:46 +0300

    master

--- Goober.EntityFramework.SqlServer/ServiceCollectionExtensions.cs
+++ Goober.EntityFramework.SqlServer/ServiceCollectionExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 
-namespace Goober.EntityFramework
+namespace Goober.EntityFramework.SqlServer
 {
     public static class ServiceCollectionExtensions
     {
