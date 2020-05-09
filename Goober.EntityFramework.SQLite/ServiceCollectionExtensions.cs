4b668d7cab04459ae31219e6ba52ff29fe875649
Вячеслав Титов <viachesl4v@outlook.com>
2020-05-09 12:30:30 +0300

    SQLite

---
+++ Goober.EntityFramework.SQLite/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Goober.EntityFramework.SQLite
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterSQLiteDbContext<TContextService, TContextImplementation>(
+            this IServiceCollection services,
+            Func<string> GetConnectionString)
+                where TContextService : class
+                where TContextImplementation : DbContext, TContextService
+        {
+            services.AddDbContext<TContextImplementation>(options =>
+            {
+                options.UseSqlite(GetConnectionString());
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<TContextService, TContextImplementation>();
+        }
+    }
+}
