9332b98e85629286893cff7b2b71e6fe5992cfa2
denis.tskhaj@yandex.ru <denis.tskhaj@yandex.ru>
2021-05-03 20:42:44 +0300

    PostgreSql

---
+++ Goober.EntityFramework.PostgreSql/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Goober.EntityFramework.PostgreSql
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterMySqlDbContext<TContextService, TContextImplementation>(
+            this IServiceCollection services,
+            Func<string> GetConnectionString)
+                where TContextService : class
+                where TContextImplementation : DbContext, TContextService
+        {
+            services.AddDbContext<TContextImplementation>(options =>
+            {
+                options.UseNpgsql(GetConnectionString());
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<TContextService, TContextImplementation>();
+        }
+    }
+}
